1fd6fdbd34da8f8404ea081593f905dea1933e70
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-09 17:17:48 +0200

    generaliza la funcion para cualquier altura

--- src/christmasTree.js
+++ src/christmasTree.js
@@ -1,8 +1,16 @@
 module.exports = function buildChristmasTree(altura) {
-  if (altura == 1) {
-    return '*'
-  } else if (altura == 2) {
-    return ' * \n***'
+  let str = ''
+  for (let i = 0; i < altura; i++) {
+    if (altura - 1 === i) {
+      str += '*'.repeat(i * 2 + 1)
+    } else {
+      str += ' '.repeat(Math.ceil((altura - i) / 2))
+      str += '*'.repeat(i * 2 + 1)
+      str += ' '.repeat(Math.ceil((altura - i) / 2))
+    }
+    if (i < altura - 1) {
+      str += '\n'
+    }
   }
-  return '  *  \n *** \n*****'
+  return str
 }
