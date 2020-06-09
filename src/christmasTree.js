9ef2018c72b5b979fd29599601c8f9f807f49275
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-09 16:14:43 +0200

    crea primer test

--- src/christmasTree.js
+++ src/christmasTree.js
@@ -1,1 +1,4 @@
 // Código para kata christmas tree
+const foo = '1'
+
+const bar = '2'
--- test/christmasTree.test.js
+++ test/christmasTree.test.js
@@ -1,1 +1,17 @@
-// Test para kata christmas tree
+describe('Christmas Tree', () => {
+  it('debería poder crear un arbol de 1 altura', () => {
+    // Arrange
+
+    // Act
+    // Usar christmasTree pasándole 1 y guardar el arbol generado
+    const christmasTree = buildChristmasTree(1)
+
+    // Assert
+    // que el arbol sea *
+    expect(christmasTree).toMatch('*')
+  })
+})
+
+function buildChristmasTree() {
+  return '*'
+}
