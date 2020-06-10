ad47005f81703f933f6f5585643680de71542fbc
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-10 16:44:46 +0200

    Refactor: naming

--- src/christmasTree.js
+++ src/christmasTree.js
@@ -1,12 +1,12 @@
 module.exports = function buildChristmasTree(altura) {
   let arbol = ''
-  for (let i = 1; i <= altura; i++) {
-    const aire = ' '.repeat(Math.ceil(altura - i))
-    const rama = '*'.repeat(i * 2 - 1)
+  for (let piso = 1; piso <= altura; piso++) {
+    const aire = ' '.repeat(Math.ceil(altura - piso))
+    const rama = '*'.repeat(piso * 2 - 1)
 
     arbol += aire + rama + aire
 
-    if (i < altura) {
+    if (piso < altura) {
       arbol += '\n'
     }
   }
