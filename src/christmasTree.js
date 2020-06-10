d4a132ad0494cd26733935e2bc2afcb4f76a1594
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-10 16:23:49 +0200

    quita numeros magicos

--- src/christmasTree.js
+++ src/christmasTree.js
@@ -1,11 +1,11 @@
 module.exports = function buildChristmasTree(altura) {
   let str = ''
-  for (let i = 0; i < altura; i++) {
-    str += ' '.repeat(Math.ceil(altura - i - 1))
-    str += '*'.repeat(i * 2 + 1)
-    str += ' '.repeat(Math.ceil(altura - i - 1))
+  for (let i = 1; i <= altura; i++) {
+    str += ' '.repeat(Math.ceil(altura - i))
+    str += '*'.repeat(i * 2 - 1)
+    str += ' '.repeat(Math.ceil(altura - i))
 
-    if (i < altura - 1) {
+    if (i < altura) {
       str += '\n'
     }
   }
