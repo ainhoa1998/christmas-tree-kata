515acd225c7d98076d18c92ce83f176f5656263a
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-10 12:26:34 +0200

    refactoriza la funcion buildChristmasTree

--- src/christmasTree.js
+++ src/christmasTree.js
@@ -1,13 +1,10 @@
 module.exports = function buildChristmasTree(altura) {
   let str = ''
   for (let i = 0; i < altura; i++) {
-    if (altura - 1 === i) {
-      str += '*'.repeat(i * 2 + 1)
-    } else {
-      str += ' '.repeat(Math.ceil((altura - i) / 2))
-      str += '*'.repeat(i * 2 + 1)
-      str += ' '.repeat(Math.ceil((altura - i) / 2))
-    }
+    str += ' '.repeat(Math.ceil(altura - i - 1))
+    str += '*'.repeat(i * 2 + 1)
+    str += ' '.repeat(Math.ceil(altura - i - 1))
+
     if (i < altura - 1) {
       str += '\n'
     }
